13e762e5345a097f0a5e81f5f65c3ae23ac8ac92
Tim Randall <trtimrandall@gmail.com>
2021-09-25 17:06:45 +1000

    Initial Commit

---
+++ HomeLoanCalculator/HomeLoanCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeLoanCalculatorApp.swift
+//  HomeLoanCalculator
+//
+//  Created by Tim Randall on 25/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HomeLoanCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
